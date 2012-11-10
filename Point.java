c9c9bbfba0ec8240c95dd1fe8d00e84a142af34f
lobiCode <urossla@gmail.com>
2012-11-10 23:52:45 +0100

    Adding compareTo and String method

--- Point.java
+++ Point.java
@@ -1,36 +1,51 @@
 /*************************************************************************
  * Name: Uros Slana
- * Email: urossla@gmail.com
+ * Email: urossla(at)gmail.com
  *
- * Compilation:  javac Point.java
+ * Compilation: javac Point.java
  *
  * Description: An immutable data type for points in the plane.
  *
  *************************************************************************/
 
-public class Point {
+public class Point implements Comparable<Point> {
 	
-	private final double x;
-	private final double y;
+	public final double x;
+	public final double y;
 
 	public Point(double x, double y) {
 		this.x = x;
 		this.y = y;
 	}
-	
-	public double x() {
-		return x;
-	}
-	
-	public double y() {
-		return y;
+
+	// comparing y-coordinates and breaking ties by x-coordinates
+    public int compareTo(Point p) {
+		//TODO try to write this as a oneliner
+		if (y < p.y) {
+			return -1;
+		}
+		if (y > p.y) {
+			return 1;
+		}
+		if (x < p.x) {
+			return -1;
+		}
+		if (x > p.x) {
+			return 1;
+		}
+		return 0;
 	}
 
-	//retrun Euclidean distance of this and p
+	//returns Euclidean distance of this and p
 	public double distanceTo(Point p) {
 		double dx = this.x - p.x;
 		double dy = this.y - p.y;
 
 		return Math.sqrt((dx*dx) + (dy*dy));
+	}
+	
+	// convert to string
+	public String toString() {
+		return "(" + x + ", " + y + ")";
 	}
 }
